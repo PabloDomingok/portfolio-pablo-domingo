53269961f3c1ec6bdc32e272b6246463694d570b
Pablo Domingo <pablodomingo1993@gmail.com>
2024-04-26 22:46:25 -0300

    Update styles

--- script.js
+++ script.js
@@ -5,17 +5,6 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-function toggleArrowVisibility() {
-  const arrow = document.querySelector('.arrow_up');
-  const profileSection = document.getElementById('profile');
-
-  // Si la sección de perfil está visible en la ventana, oculta la flecha; de lo contrario, muestre la flecha.
-  if (isElementInViewport(profileSection)) {
-    arrow.style.display = 'none';
-  } else {
-    arrow.style.display = 'block';
-  }
-}
 
 // Función para verificar si un elemento está visible en la ventana del navegador
 function isElementInViewport(el) {
@@ -28,21 +17,31 @@ function isElementInViewport(el) {
   );
 }
 
+function toggleArrowVisibility() {
+  const arrow = document.querySelector('.arrow_up');
+  const desktopNav = document.getElementById('desktop-nav');
+
+  if (isElementInViewport(desktopNav)) {
+    arrow.classList.add('hidden'); // Agrega la clase hidden cuando el nav está visible
+  } else {
+    arrow.classList.remove('hidden'); // Remueve la clase hidden cuando el nav no está visible
+  }
+}
+// Obtener la imagen con la clase .profile-picture
+const profilePicture = document.querySelector('.profile-picture');
+
+// Agregar un controlador de eventos de clic a la imagen
+profilePicture.addEventListener('click', redirectToAbout);
+
+// Definir la función para redirigir a 'about me'
+function redirectToAbout() {
+  // Redirigir a la sección 'about'
+  window.location.href = '#about';
+}
+
+
 // Agrega un event listener para escuchar el evento de desplazamiento y llamar a la función toggleArrowVisibility
 window.addEventListener('scroll', toggleArrowVisibility);
 
 // Llama a la función toggleArrowVisibility inicialmente para verificar la visibilidad de la flecha al cargar la página
 toggleArrowVisibility();
-
-function toggleArrowVisibility() {
-  const arrow = document.querySelector('.arrow_up');
-  const profileSection = document.getElementById('profile');
-
-  if (isElementInViewport(profileSection)) {
-    arrow.classList.add('hidden'); // Agrega la clase hidden cuando la sección profile está visible
-  } else {
-    arrow.classList.remove('hidden'); // Remueve la clase hidden cuando la sección profile no está visible
-  }
-}
-
-
